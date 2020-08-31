c07395250f211d0d0c9afdeece66fb6294bcd64c
Alexey Rubtsov <Alexey_Rubtsov@epam.com>
2020-08-31 16:16:53 +0300

    Use "jq-web" for all tests

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
-import { test_with_jq_web, test_with_node_jq } from './test_helpers.js'
+import { test_with_jq_web } from './test_helpers.js'
 const { tests_jq_web, tests_node_jq } = require('json-loader!./tests.json')
 
 tests_jq_web.forEach(test_with_jq_web)
-tests_node_jq.forEach(test_with_node_jq)
+tests_node_jq.forEach(test_with_jq_web)
 
 
